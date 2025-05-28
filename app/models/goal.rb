786be565a71d74b4c643e464a60f4ed083569b1e
dylan akpan <dakpan0593@gmail.com>
2025-05-28 15:16:34 +0100

    navbar

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,16 @@
 class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+
+
+   def index
+    @goals = Goal.all
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+
 end
