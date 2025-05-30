bd605becb44a59af91e06ea01b016555da9744b6
dylan akpan <dakpan0593@gmail.com>
2025-05-30 14:03:42 +0100

    delete

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :group_memberships
+  has_many :group_memberships, dependent: :destroy
   has_many :members, through: :group_memberships, source: :user
-  has_many :goals
+  has_many :goals, dependent: :destroy
 end
