dbb355dbd57dbfdb97d09c425e4ff7bf9289f7fb
Manami Tsukada <manami.tsukada3@gmail.com>
2025-05-30 13:46:34 +0100

    added stikersheet

--- app/controllers/goal_completions_controller.rb
+++ app/controllers/goal_completions_controller.rb
@@ -1,2 +1,29 @@
 class GoalCompletionsController < ApplicationController
+
+  before_action :set_group
+
+  def update_progress
+    day = params[:day].to_i
+    completed = ActiveModel::Type::Boolean.new.cast(params[:completed])
+    user = current_user
+
+    goal = @group.goals.first 
+
+    if completed
+      GoalCompletion.find_or_create_by(user: user, goal: goal, date: goal.start_date + day.days)
+    else
+      GoalCompletion.where(user: user, goal: goal, date: goal.start_date + day.days).destroy_all
+    end
+
+    render json: { success: true }
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
+
+
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,15 +3,15 @@ class Goal < ApplicationRecord
   belongs_to :group
   validates :name, presence: true
   validates :reason, presence: true
+validates :start_date, presence: true
 
 
+  #  def index
+  #   @goals = Goal.all
+  # end
 
-   def index
-    @goals = Goal.all
-  end
-
-  def show
-    @goal = Goal.find(params[:id])
-  end
+  # def show
+  #   @goal = Goal.find(params[:id])
+  # end
 
 end
