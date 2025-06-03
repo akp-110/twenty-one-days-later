e60d9f1db28c03efb7e9ba8ae4d13cb41a59edc3
Anil Patel <patelak1@gmail.com>
2025-06-03 10:33:08 +0100

    added styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ puts "Creating Users..."
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   username = Faker::Internet.username
-  email = Faker::Internet.unique.email
+  email = Faker::Internet.email(domain: '@lewagon.com')
   password = Faker::Internet.password(min_length: 8)
 
   User.create!(
